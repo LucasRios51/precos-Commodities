f3af3a5a0f7bff44a18bbd1487fe038143b05a41
LucasRios <lucasriosbarcelos1996@gmail.com>
2024-12-10 17:09:38 -0300

    load feito

--- src/extract_load.py
+++ src/extract_load.py
@@ -4,6 +4,17 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
 def buscar_dados(simbolo, periodo='5d', intervalo='1d'):
     ticker = yf.Ticker(simbolo)
     dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
@@ -17,8 +28,11 @@ def buscar_todos_os_dados(commodities):
         todos_os_dados.append(dados)
     return pd.concat(todos_os_dados)
 
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists= 'append', index=True, index_label='Date', schema=schema)
+
 
 if __name__ == "__main__":
     commodities = ['CL=F', 'GC=F', 'SI=F']
     dados_concatenados = buscar_todos_os_dados(commodities)
-    print(dados_concatenados)
+    salvar_no_postgres(dados_concatenados, schema='public')
